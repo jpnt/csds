c3700078abbfaf8796aefa3a5507ed95c80677bd
Joao Pinto <joaopintojpgp@gmail.com>
2024-06-20 03:51:06 +0100

    datatypes.h initial commit
    Bunch of types to use across all structs code and not use external
    libraries, altough here sys/types.h is used (optionally).

---
+++ include/csds/datatypes.h
@@ -0,0 +1,34 @@
+#ifndef CSDS_DATA_TYPES_H
+#define CSDS_DATA_TYPES_H
+
+#define __D_INCLUDE_SYS_TYPES
+
+#ifndef NULL
+#ifdef __cplusplus
+#define NULL 0 /* for C++ */
+#else
+#define NULL (void*)0 /* for C */
+#endif /* !__cplusplus */
+#endif /* !NULL */
+
+/* more explicit types */
+typedef unsigned int uint;
+typedef signed char int8;
+typedef unsigned char uint8;
+typedef signed short int16;
+typedef unsigned short uint16;
+typedef signed int int32;
+typedef unsigned int uint32;
+typedef float real32;
+typedef double real64;
+#ifdef __D_INCLUDE_SYS_TYPES
+#include <sys/types.h>
+typedef __int64_t int64;
+typedef __uint64_t uint64;
+#endif /* !__D_INCLUDE_SYS_TYPES */
+
+#ifndef PI
+#define PI 3.141592653
+#endif /* !PI */
+
+#endif /* !CSDS_DATA_TYPES_H */
