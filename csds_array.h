0f62b78287d1c8f94b2f3454654ca3f819767349
João Pinto <joaopintojpgp@gmail.com>
2024-07-08 00:58:58 +0100

    more work on csds_array.h
    First something to work. Then look on how to integrate with existing
    Vec. Dont want to reivent the wheel. Simplify everything.

--- csds_array.h
+++ csds_array.h
@@ -6,12 +6,12 @@
 #define CSDS_ARRAY_H
 
 #include <stddef.h>
+#include <stdio.h>
 
 #ifndef CSDS_ARRAY_INITIAL_CAP
 #define CSDS_ARRAY_INITIAL_CAP 4
 #endif /* !CSDS_ARRAY_INITIAL_CAP */
 
-/* concern: after realloc/grow can we get the header? */
 typedef struct {
 	size_t len;
 	size_t cap;
@@ -23,10 +23,54 @@ typedef struct {
 #define CSDS_ARRAY_IMPL
 #ifdef CSDS_ARRAY_IMPL
 
-/* Get the header from the data pointer */
-#define CSDS_ARRAY_HEADER_OF(ptr) ((ArrayHeader*)((char*)(ptr) - offsetof(ArrayHeader, data)))
-#define array_len(arr) ((arr) ? CSDS_ARRAY_HEADER_OF(arr)->len : 0)
-#define array_cap(arr) ((arr) ? CSDS_ARRAY_HEADER_OF(arr)->cap : 0)
-#define array_dealloc(arr) ((arr) ? (free(CSDS_ARRAY_HEADER_OF(arr)), (arr)), (arr) = NULL : (void)0)
+/* Get the array header from the array pointer */
+#define CSDS_ARRAYHEADER_OF(arr) ((ArrayHeader*)((char*)(arr)-sizeof(ArrayHeader)))
+
+/* Get the length of the array by the array pointer */
+#define array_len(arr) ((arr) ? CSDS_ARRAYHEADER_OF(arr)->len : 0)
+
+/* Get the capacity of the array by the array pointer */
+#define array_cap(arr) ((arr) ? CSDS_ARRAYHEADER_OF(arr)->cap : 0)
+
+/* Allocate an array in the heap */
+/* TODO */
+/* #define array_alloc(arr, cap) do{\ */
+/* 	ArrayHeader* header = NULL;\ */
+/* 	if ((cap) <= 0) {\ */
+/* 		(cap) = CSDS_ARRAY_INITIAL_CAP;\ */
+/* 	header = (ArrayHeader*)malloc(sizeof(ArrayHeader)+(cap)*sizeof((arr)[0]));\ */
+/* 	if (header == NULL) {\ */
+/* 		fprintf(stderr, "Error: array_alloc: Could not allocate memory")\ */
+/* 		exit(1);\ */
+/* 	}\ */
+/* 	header->cap = (cap);\ */
+/* 	header->len = 0;\ */
+/* 	(arr) = (typeof(arr))header+1;\ */
+/* 	return header;\ */
+/* } while(0) */
+
+/* #define array_alloc(arr, cap) do { \ */
+/*     ArrayHeader* header = NULL; \ */
+/* 	int cap = (cap);\ */
+/*     if ((cap) <= 0) { \ */
+/*         cap = CSDS_ARRAY_INITIAL_CAP; \ */
+/*     } \ */
+/*     header = (ArrayHeader*)malloc(sizeof(ArrayHeader) + (cap) * sizeof((arr)[0])); \ */
+/*     if (header == NULL) { \ */
+/*         fprintf(stderr, "Error: array_alloc: Could not allocate memory\n"); \ */
+/*         exit(1); \ */
+/*     } \ */
+/*     header->cap = cap; \ */
+/*     header->len = 0; \ */
+/*     (arr) = (typeof(arr))(header + 1); \ */
+/* } while(0) */
+
+/* Deallocate/Free array header memory and corresponding data */
+#define array_dealloc(arr) do{\
+	if (arr) {\
+		free(CSDS_ARRAYHEADER_OF(arr));\
+		(arr) = NULL;\
+	}\
+} while(0)
 
 #endif /* CSDS_ARRAY_IMPL */
--- tests/test_array.c
+++ tests/test_array.c
@@ -6,41 +6,89 @@
 
 /* quick and dirty testing before actual implementation */
 void test1() {
+	/*
+	 * GOAL:
+	 *
+	 * int* arr = NULL;
+	 * array_push(arr, 10);
+	 * assay_ins(arr, 1, 20);
+	 * assert(array_pop(arr) == 20);
+	 * assert(arr[0] == 10);
+	 * ...
+	 */
+
+
 	int INITIAL_CAPACITY=4;
 	int i;  /*, cap = 4, len = 0;*/
-	int elem_size = sizeof(int); /* first problem: get the size of the elements in a generic way */
+	/* size_t cap, len; */
+	/* int elem_size = sizeof(int); */
 	int* arr = NULL;
 	ArrayHeader* ah;
+	
+	printf("===============================\n");
+	printf("DEBUG INFO:\n");
+	printf("-------------------------------\n");
+	printf("sizeof(ArrayHeader)=%lu\n",sizeof(ArrayHeader));
+	printf("-------------------------------\n");
+	printf("first problem: get size of the elements of the array\n");
+	/* attempt 1: this just gives us the poiter (8B for 64bits) / by sizeof(int) 4 = 2 */
+	printf("sizeof(arr)/sizeof(arr[0])=%lu\n", sizeof(arr)/sizeof(arr[0]));
+	/* attempt 2: get the sizeof the first element */
+	printf("sizeof(arr[0])=%lu\n", sizeof(arr[0]));
+	printf("===============================\n");
+
 
 	/* |_cap_|_len_|_elem1_|_elem2_|_elemN_| */
-	ah = (ArrayHeader*) malloc(sizeof(ArrayHeader) + INITIAL_CAPACITY * sizeof(elem_size)); /* len + cap */
+	ah = (ArrayHeader*) malloc(sizeof(ArrayHeader) + INITIAL_CAPACITY * sizeof(arr[0])); /* len + cap */
+	if(!ah) {
+		perror("Error");
+		exit(1);
+	}
 	ah->cap = INITIAL_CAPACITY;
 	ah->len = 0;
 
 	/* arr = malloc(cap * sizeof(elem_size)); */
-	arr = (int*)ah+sizeof(ArrayHeader);
+
+	/* assembly style */
+	/* arr = (int*)((char*)ah+sizeof(ArrayHeader)); */
+
+	/* C style: think: pointer is address + type */
+	/* problem: how to we create a generic function that casts to (T*) */
+	arr = (int*)(ah+1);
 
 
 	/* access cap and len though the array */
+	ArrayHeader* ah_from_arr = CSDS_ARRAYHEADER_OF(arr);
 	
 	/* fill up array */
-	for (i=0; i<ah->cap; ++i) {
+	for (i=0; i<ah_from_arr->cap; ++i) {
 		arr[i] = i+1;
-		ah->len++;
+		ah_from_arr->len++;
 	}
 
 	/* print values */
-	for (i=0;i<cap;++i){
+	for (i=0;i<ah_from_arr->len;++i){
 		ASSERT_EQUALS(i+1,arr[i]);
 		printf("%d\n",arr[i]);
 	}
 
-	ASSERT_EQUALS(len, 4);
+	ASSERT_EQUALS(ah_from_arr->len, 4);
+
+	array_dealloc(arr);
+}
+
+
+void test2() {
+	int* arr = NULL;
+	array_alloc(arr, 0);
+
+	ASSERT(arr != NULL, "arr should not be NULL at this point");
 
 }
 
 int main(void) {
-	TEST_RUN(test1, "test1");
+	/* TEST_RUN(test1, "test1"); */
+	TEST_RUN(test2, "test2");
 
 	TEST_REPORT();
 }
